ee4cbd3631cd0e03d021f2b0bf1c2160ff28e87d
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-21 21:17:10 -0500

    Liverpool is ready.

---
+++ lib/liverpool.rb
@@ -0,0 +1,26 @@
+# liverpool.rb
+class Liverpool
+
+  def initialize(name, district, generation)
+    @name="S. Gerrard"
+    @district="Toxteth"
+    @generation="3rd"
+  end
+
+  def age
+    @age
+  end
+
+  def industry
+    @industry
+  end
+
+  def red_or_blue
+    @red_or_blue
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+  end
+
+end
