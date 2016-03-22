3954d10e7d97d21f212ff85a24d6cae900dd36c1
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-22 09:48:37 -0500

    Updated to fill in gaps

--- lib/unicorn.rb
+++ lib/unicorn.rb
@@ -1,12 +1,45 @@
 # unicorn.rb
+
+# The intent is to collect as much data on known Unicorns as possible.
+
+# Starts off initializing the first three fields:
+#  registered, is the Unicorn officially registered;
+#  in_service, how long has the Unicorn been in service;
+#  and last_tour, when was the Unicorn's last tour.
+
+# The next three fields make use of Getters to collect info on:
+#  name, what is the Unicorn's commonly used name;
+#  powers, what special powers does the Unicorn utilize;
+#  and country_of_origin, from whence does the Unicorn come.
+
+# The last field uses a Setter to identify the Unicorn's rank.
+
 class Unicorn
 
-  def initialize(registered, in_service, last_tour)
-    @registered="Yes"
-    @in_service="Since 1979"
-    @last_tour="Grant Park August 2001"
+  def initialize(registered, in_service, last_tour, name, powers, country_of_origin, rank)
+
+    @registered       =registered
+    @in_service       =in_service
+    @last_tour        =last_tour
+    @name             =name
+    @powers           =powers
+    @country_of_origin=country_of_origin
+    @rank             =rank
   end
 
+  def registered?
+    @registered
+  end
+
+  def in_service
+    @in_service
+  end
+
+  def last_tour
+    @last_tour
+  end
+
+  end
   def name
     @name
   end
