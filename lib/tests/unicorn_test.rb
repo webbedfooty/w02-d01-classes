23e85ffb8ee876f6a2ec8dca141e6d926e1fdc6e
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-23 16:57:54 -0500

    tweek

--- lib/tests/unicorn_test.rb
+++ lib/tests/unicorn_test.rb
@@ -1,5 +1,11 @@
-require "test_helper"
+require_relative "test_helper"
+require_relative "../unicorn.rb"
 
 class UnicornTest < Minitest::Test
 
+  def test_under_normal_conditions
+    unicorn = Unicorn.new(name: "Titus", powers: "Ice",
+    country: "Russia", rank: "General")
+    refute_nil(unicorn)
+  end
 end
